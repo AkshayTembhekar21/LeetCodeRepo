9220f27ddbd7fdc0301bd80e38daf9822629ce0c
AkshayTembhekar21 <akshaytembhekar@gmail.com>
2022-09-14 17:47:26 +0530

    Non Repeating Longest SubString - LeetCode

--- NonRepeatingLongestSubstring.java
+++ NonRepeatingLongestSubstring.java
@@ -1,37 +1,19 @@
-class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int totalLen = nums1.length + nums2.length;
-        int i = 0;
-        int j = 0;
-        int k = 0;
-        int res[] = new int[totalLen];
-        while (i < nums1.length && j < nums2.length) {
-            if (nums1[i] <= nums2[j]) {
-                res[k] = nums1[i];
-                i++;
-                k++;
-            } else {
-                res[k] = nums2[j];
-                j++;
-                k++;
-            }
-        }
-        while (i<nums1.length){
-            res[k] = nums1[i];
-            i++;
-            k++;
-        }
-        while (j< nums2.length) {
-            res[k] = nums2[j];
-            j++;
-            k++;
-        }
-        
-        if (totalLen%2!=0){
-            return res[(totalLen/2)];
-        } else {
-            return (res[totalLen/2] + res[(totalLen/2)-1])/2f;
-        }
-    }
+import java.util.HashSet;
 
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int res = 0;
+        int j = 0;
+        HashSet<Character> set = new HashSet<>();
+        
+        for(int i=0; i<s.length() && j<s.length(); i++){
+            while(set.contains(s.charAt(i))){
+                set.remove(s.charAt(j));
+                j++;
+            }
+            set.add(s.charAt(i));
+            res = Math.max(res, set.size());        
+        }
+        return res;
+    }
 }
